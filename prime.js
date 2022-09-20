03503e3945e6a7700faf0b0eef1ad1d0541547c6
imSAJJAKALI <sajjak2506@gmail.com>
2022-09-20 13:12:17 +0530

    code fix sjk2

--- prime.js
+++ prime.js
@@ -1,4 +1,5 @@
-let num=13;
+
+function prime(num){
 let count=0;
 for(let i=0; i<=num; i++){
   if(num%i==0){
@@ -10,4 +11,6 @@ if(count==2){
    }else{
    console.log("not prime")
 }
+}
 
+prime(num);
